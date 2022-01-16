6a6e0e77edf12d73c5d92d58b70a31322a7f4bcb
catwen <jquery.min.js@gmail.com>
2022-01-16 12:06:02 +0800

    feat: add pop

--- wz-algo/stack/linked-list.spec.ts
+++ wz-algo/stack/linked-list.spec.ts
@@ -8,3 +8,14 @@ test('push', () => {
   expect(stack.head.next?.next?.name).toEqual('b')
   expect(stack.top?.name).toEqual('b')
 })
+
+test('pop', () => {
+  const stack = new Stack()
+  stack.push('a')
+  stack.push('b')
+
+  const node = stack.pop()
+  expect(node?.name).toEqual('b')
+  expect(stack.head.next?.name).toEqual('a')
+  expect(stack.head.next?.next).toBeFalsy()
+})
