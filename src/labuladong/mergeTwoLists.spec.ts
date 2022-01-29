3168de3a7e9208af9f95167c1da4094ddce39933
catwen <jquery.min.js@gmail.com>
2022-01-29 16:06:02 +0800

    feat: update test case

--- src/labuladong/mergeTwoLists.spec.ts
+++ src/labuladong/mergeTwoLists.spec.ts
@@ -6,13 +6,15 @@ test('success', () => {
   linkedList1.append('1')
   linkedList1.append('3')
   linkedList1.append('5')
+  linkedList1.append('6')
 
   const linkedList2 = new LinkedList()
   linkedList2.append('2')
   linkedList2.append('4')
-  linkedList2.append('6')
+  linkedList2.append('7')
+  linkedList2.append('8')
 
   const newList = mergeTwoLists(linkedList1, linkedList2)
   const displayNewList = newList.display()
-  expect(displayNewList).toEqual(['1', '2', '3', '4', '5', '6'])
+  expect(displayNewList).toEqual(['1', '2', '3', '4', '5', '6', '7', '8'])
 })
